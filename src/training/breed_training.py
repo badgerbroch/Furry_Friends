fbc1b524ffead02ef174ee8a3ca823fbc0c4161d
Drew Broch <abroch@wisc.edu>
2025-12-09 14:06:26 -0600

    Breed training updated training params

--- src/training/breed_training.py
+++ src/training/breed_training.py
@@ -1,10 +1,25 @@
 from ultralytics import YOLO
+
 if __name__ == "__main__":
     model = YOLO("yolo11s-cls.pt")
+    
+    
     model.train(
         data="pets_breed_split",
-        epochs=50,
+        epochs=120,
+        patience=20,
         imgsz=256,
-        batch=64,
-        amp=True
+        batch=32,
+        optimizer="AdamW", # Adam over SGD since small dataset
+        device=0,
+        lr0=1e-3, # smaller lr since small dataset
+        lrf=0.01,
+        label_smoothing=0.1,
+        weight_decay=0.01,
+        dropout=0.2,
+        
+        workers=4,
+        cache=True,
+        val=True,
+        seed=0,
     )
